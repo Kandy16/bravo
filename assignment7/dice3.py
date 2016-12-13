4bffed985c4b93573cd6114b699887aced45fe00
DaniA7 <akbari@uni-koblenz.de>
2016-12-13 21:31:22 +0100

    Add files via upload

---
+++ assignment7/dice3.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 12 15:22:46 2016
+
+@author: Daniel
+"""
+
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from pylab import xticks
+
+
+min=1
+
+max=6
+
+roll = 0
+
+die1=list()
+die2=list()
+
+while (roll<101):
+
+    die1.append(random.randint(min,max))
+
+    die2.append(random.randint(min,max))
+    
+    
+   
+    
+    roll += 1
+Sum = [x+y for x,y in zip(die1,die2)]
+ 
+   
+def Histogram(x):
+   
+    plt.figure(figsize=(8, 6), dpi=80)
+    plt.hist(x, 60, alpha=0.85)
+    plt.title('Histogram')
+    plt.xlim(0,14)
+    median =np.median(x)
+    plt.axvline(median, color='r', linestyle='dashed', linewidth=3)
+    xticks(range(2, 13))
+    plt.grid(True)
+    plt.show()
+    
+    
+    
+def Cumulative (x):
+    
+    sort = np.sort(x)
+    
+    cdf = np.cumsum(sort)
+   # ncdf = 1.0/cdf[-1] * cdf
+
+    median =np.median(x)
+    #print(median)
+    #print(x)
+    plt.figure(figsize=(8, 6), dpi=80)
+    #plt.plot(ncdf,c='b')
+
+    num_bins = 10
+    counts, bin_edges = np.histogram(x, bins=num_bins, normed=True)
+    cdf = np.cumsum(counts)
+    plt.plot(bin_edges[1:], cdf)
+    
+    #plt.plot(base[:1], x, c='blue')
+    plt.ylabel('CDF')
+    #plt.hist(x, 60, alpha=0.85)
+    #xticks(range(2, 13))
+    plt.grid(True)
+    plt.axvline(median, color='r', linestyle='dashed', linewidth=3)
+    plt.axvline(9, color='g', linewidth=3)
+    #plt.xlim(0,1)
+   
+    plt.legend()
+    plt.show()
+    
+    
+        
+#dist = np.linalg.norm(a-b)
+
+
+Histogram(Sum)
+
+Cumulative(Sum)
+
+
+
+
+
+
+
+
